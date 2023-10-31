93465a0a0042e64827284fed762c6524343ec15f
abhayychaudhary <abhayjan201@gmail.com>
2023-10-31 22:30:05 +0530

    created a version

---
+++ src/main/java/mypkg/TestCal.java
@@ -0,0 +1,19 @@
+package mypkg;
+
+public class TestCal {
+	public int sum(int a, int b)
+	{
+		return(a+b);
+	}
+	public int diff(int a,int b)
+	{
+		return(a-b);
+	}
+	public static void main(Sting[] args)
+	{
+		TestCal a= new TestCal();
+		System.out.println("Sum:"+a.sum(10,3));
+		System.out.println("Diff:"+a.diff(10,3));
+	}
+
+}
